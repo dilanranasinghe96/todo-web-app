3d46968ed50ad1b700427de48b857e8f36e4e8f6
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2025-01-19 10:05:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My Todos</h1>
+     <div className='todo-wrapper'>
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type='text' placeholder='What is the title?'/>
+          </div>
+
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type='text' placeholder='What is the description?'/>
+          </div>
+
+          <div className='todo-input-item'>
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+        </div>
+      <div className='btn-area'>
+        <button>Todo</button>
+        <button>Completed</button>
+      </div>
+      <div className='todo-list'>
+        <div className='todo-list-item'>
+            <h1>Task 1</h1>
+            <p>Description</p>
+        </div>
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
